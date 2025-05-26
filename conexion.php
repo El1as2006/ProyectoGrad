e7c574a32443a0847102f7fa0cf1bb1ba11db65c
El1as2006 <131718472+El1as2006@users.noreply.github.com>
2025-05-25 23:08:04 -0600

    varios cambios

--- conexion.php
+++ conexion.php
@@ -12,7 +12,7 @@ $options = [
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $pass, $options);
-    return $pdo; // 👈 ESTA LÍNEA ES CLAVE
+    return $pdo; 
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
